a6dca2844ea1b549954510aa9172f6b06e1a06bd
Julian Herrero <microweb10@gmail.com>
2023-07-05 11:16:52 +0200

    Add custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,17 @@ Rails.application.routes.draw do
   get "help/how-to-use",  to: "pages#show", id: "help/how_to_use/index",  as: "how_to_use"
   get "help/faq",         to: "pages#show", id: "faq",                    as: "faq"
 
+  # Custom routes
+  get "stemvantenboer", to: redirect("/budgets/10")
+  get "garmerwolde", to: redirect("/budgets/10/investments?heading_id=8")
+  get "lellens", to: redirect("/budgets/10/investments?heading_id=9")
+  get "sintannen", to: redirect("/budgets/10/investments?heading_id=10")
+  get "tenboer", to: redirect("/budgets/10/investments?heading_id=11")
+  get "thesinge", to: redirect("/budgets/10/investments?heading_id=12")
+  get "winneweer", to: redirect("/budgets/10/investments?heading_id=13")
+  get "wittewierum", to: redirect("/budgets/10/investments?heading_id=14")
+  get "tenpost", to: redirect("/budgets/10/investments?heading_id=15")
+  get "woltersum", to: redirect("/budgets/10/investments?heading_id=16")
   get "beijumbruist", to: redirect("/budgets/18")
   get "lewenborgleeft", to: redirect("/budgets/19")
   get "beweegplekmikkelhorst", to: redirect("/budgets/20")
