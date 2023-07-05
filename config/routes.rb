85b6b7de73fde74f141d4cfdcd0c7b1100dd5031
Julian Herrero <microweb10@gmail.com>
2023-07-05 11:14:25 +0200

    Refactor - Remove usused code

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,6 @@ Rails.application.routes.draw do
   resources :remote_translations, only: [:create]
 
   # More info pages
-  help_page_id = Rails.env.test? ? "help/index" : "vragen"
   get "help",             to: "pages#show", id: "help/index",             as: "help"
 
   get "help/how-to-use",  to: "pages#show", id: "help/how_to_use/index",  as: "how_to_use"
